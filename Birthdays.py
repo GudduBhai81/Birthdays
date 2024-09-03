e88ee973938fd410374f9a1b637cfc6fc491020a
GudduBhai81 <306botala@gmail.com>
2024-09-03 10:14:59 -0700

    Add files via upload

---
+++ Birthdays.py
@@ -0,0 +1,20 @@
+x = 2011
+f = "Abdullah's birthday year is"
+g = 2011
+h = "Kaneez Fatima's birthday year is"
+m = 2013
+d = "Ayesha's birthday year is"
+s = 2015
+c = "Hasnain's birthday year is"
+q = 2023
+w = "Aina's birthday year is"
+print(f)
+print(x)
+print(h)
+print(g)
+print(d)
+print(m)
+print(c)
+print(s)
+print(w)
+print(q)
